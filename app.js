f02e3ac7ca4980ff2aeac556142470cfca3a0c30
Takeshi32 <90292423+Takeshi32@users.noreply.github.com>
2024-07-01 04:05:02 -0400

    aaa

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoutes = require('./src/routes/userRoutes'); // Asegúrate de ajustar la ruta
+const userRoutes = require('./src/routes/userRoutes'); 
+const authRoutes = require('./src/routes/authRoutes'); 
+const logRoutes = require('./src/routes/logRoutes'); 
+const reportRoutes = require('./src/routes/reportRoutes'); 
+const reservationRoutes = require('./src/routes/reservationRoutes'); 
+const tableRoutes = require('./src/routes/tableRoutes'); 
 const { initialize, close } = require('./src/config/database');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -10,13 +15,18 @@ const app = express();
 
 // Configura CORS
 app.use(cors({
-  origin: 'http://localhost:3000', // Cambia esto por el origen de tu frontend
+  origin: 'http://localhost:3000', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/log', logRoutes);
+app.use('/api/report', reportRoutes);
+app.use('/api/reservation', reservationRoutes); // Cambio: Corregir la ruta de reservas
+app.use('/api/table', tableRoutes); // Cambio: Corregir la ruta de mesas
 
 const PORT = process.env.PORT || 5000;
 
@@ -36,5 +46,3 @@ process.on('SIGTERM', () => {
 });
 
 module.exports = app;
-
-
