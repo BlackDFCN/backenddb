637df284244867f7857c2196c6ef560286aa61e2
Takeshi32 <90292423+Takeshi32@users.noreply.github.com>
2024-07-01 04:10:47 -0400

    aaa

--- src/config/database.js
+++ src/config/database.js
@@ -4,8 +4,12 @@ const path = require('path');
 
 async function initialize() {
   try {
+    // Establecer la variable de entorno TNS_ADMIN
+    process.env.TNS_ADMIN = path.resolve('C:\\Users\\Alvaro Bustos\\Desktop\\BaseDeDAtos\\backenddb\\Wallet_UCMDB');
+
     // Inicializar el cliente Oracle con la ruta correcta
     oracledb.initOracleClient({ libDir: 'C:\\Users\\Alvaro Bustos\\Desktop\\instantclient-basiclite-windows.x64-23.4.0.24.05\\instantclient_23_4' });
+
     await oracledb.createPool({
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
@@ -51,5 +55,3 @@ async function execute(query, binds = [], options = {}) {
 }
 
 module.exports = { initialize, close, execute };
-
-
